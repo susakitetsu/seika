e58e9cdecd07bd36a90df934c813f1747623e381
Tetsu Susaki <susaki@cosmoway.net>
2015-09-03 12:14:54 +0900

    少しの動きで反応するように改良しました

--- kasokudo のコピー/app/src/main/java/com/example/tetsususaki/kasokudo/MainActivity.java
+++ kasokudo のコピー/app/src/main/java/com/example/tetsususaki/kasokudo/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.tetsususaki.kasokudo;
 
-        import java.util.List;
-        import android.app.Activity;
-        import android.content.Context;
-        import android.hardware.Sensor;
-        import android.hardware.SensorEvent;
-        import android.hardware.SensorEventListener;
-        import android.hardware.SensorManager;
-        import android.os.Bundle;
-        import android.widget.TextView;
+import java.util.List;
+
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements SensorEventListener {
-    /** Called when the activity is first created. */
-    int i[]={0,0,0},i2=0,i4=0,j=0;
-    String text2="";
+    /**
+     * Called when the activity is first created.
+     */
+    private int  i2 = 0, i4 = 0, state = 0;
+    private double kari[]={0,0,0};
+    private double i[] = {0, 0, 0},atai[]={0,0,0};
+    private final int StateA=0,StateB=1,StateC=2,StateD=3;
+    String text2 = "";
     private SensorManager manager;
     private Sensor sensor;
     private TextView textView;
@@ -43,6 +51,7 @@ public class MainActivity extends Activity implements SensorEventListener {
                     SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
+
     @Override
     protected void onStop() {
         super.onStop();
@@ -62,33 +71,52 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     private void showValues(float[] values) {
 
-        String text = "" +
-                "values[0]:" + values[0] + "\n" +
-                "values[1]:" + values[1] + "\n" +
-                "values[2]:" + values[2] + "\n";
-
-        if(j==0){
-            for(i2=0;i2<3;i2++) {
-                i[i2] = (int) values[i2];
+        if(state==StateA){//加速度センサーの初期値を取る
+            for (i2 = 0; i2 < 3; i2++) {
+                i[i2] = values[i2];
+                atai[i2]=i[i2];
             }
-            text2="停止";
-            j=1;
-        }else if(j==1){
-            for(i2=0;i2<3;i2++) {
-                if (i[i2] + 3 < (int) values[i2] || i[i2] - 3 > (int) values[i2]) {
+            text2 = "停止";
+            state = StateB;
+        } else if (state == StateB) {//加速度センサーの値が変わったかどうか
+            for (i2 = 0; i2 < 3; i2++) {
+                atai[i2]=atai[i2]*0.95+values[i2]*0.05;
+                if (i[i2] + 0.05 < atai[i2] || i[i2] - 0.05 > atai[i2]) {
                     text2 = "動作中";
-                    j = 2;
+                    state = StateC;
+                    for(i2=0;i2<3;i2++){
+                        kari[i2]=atai[i2];
+                    }
                 }
             }
-        }else{
-            i4++;
-            if(i4>10){
-                j=0;
-                i4=0;
+        } else if(state==StateC){//値が連続で変わらなかった場合D
+            for (i2 = 0; i2 < 3; i2++) {
+                atai[i2] = atai[i2] * 0.9 + values[i2] * 0.1;
             }
+            if (kari[2] + 0.03 < atai[2] || kari[2] - 0.03 > atai[2]){
+                i4=0;
+                kari[2]=atai[2];
+            }else{
+                i4++;
+            }
+            if (i4 > 5) {
+                state = StateD;
+                i4 = 0;
+            }
+        }else if (state == StateD) {//表示を元に戻す
+            for (i2 = 0; i2 < 3; i2++) {
+                i[i2] = values[i2];
+                atai[i2]=i[i2];
+            }
+            text2 = "停止";
+            state = StateB;
         }
+        String text = "" +
+                "values[0]:" + atai[0] + "\n" +
+                "values[1]:" + atai[1] + "\n" +
+                "values[2]:" + atai[2] + "\n";
 
-        textView.setText(text+text2);
+        textView.setText(text2);
 
     }
 }
